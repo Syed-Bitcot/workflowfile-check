4011b8381a0b8f85bf19efe9c220a32cfde421cc
Syed Mujeeb <68010192+Syed-Bitcot@users.noreply.github.com>
2024-06-20 22:09:56 +0530

    Update set.py

--- set.py
+++ set.py
@@ -2,9 +2,10 @@ import os
 
 print('-> Python script ran')  # This is just to verify if the script ran or not
 
+a=True
 # Simulate condition where GITHUB_ENV=true is appended to $GITHUB_ENV
 with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
-    env_file.write('GITHUB_ENV=false\n')
+    env_file.write(f'GITHUB_ENV={a}\n')
 
 # Uncomment this line to simulate the condition where GITHUB_ENV=false is appended to $GITHUB_ENV
 # with open(os.getenv('GITHUB_ENV'), 'a') as env_file:

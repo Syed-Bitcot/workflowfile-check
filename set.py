adbd89d4541b56e0306732ee0535ba7f54618c53
santhakumar <santhakumar@bitcot.com>
2024-06-20 15:19:55 +0530

    setpy file added

--- set.py
+++ set.py
@@ -2,8 +2,10 @@ import os
 
 print('-> Python script ran')  # This is just to verify if the script ran or not
 
-# Simulate condition where name=true is appended to $GITHUB_ENV
-os.system('echo "name=true" >> $GITHUB_ENV')
+# Simulate condition where GITHUB_ENV=true is appended to $GITHUB_ENV
+with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
+    env_file.write('GITHUB_ENV=true\n')
 
-# Uncomment this line to simulate the condition where name=true is not appended to $GITHUB_ENV
-# os.system('echo "name=false" >> $GITHUB_ENV')
+# Uncomment this line to simulate the condition where GITHUB_ENV=false is appended to $GITHUB_ENV
+# with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
+#     env_file.write('GITHUB_ENV=false\n')

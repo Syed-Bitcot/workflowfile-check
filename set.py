8100897e7cbd503e39409d9aaa15152ff24e2f39
santhakumar <santhakumar@bitcot.com>
2024-06-20 15:24:42 +0530

    setpy file modified- false version

--- set.py
+++ set.py
@@ -4,7 +4,7 @@ print('-> Python script ran')  # This is just to verify if the script ran or not
 
 # Simulate condition where GITHUB_ENV=true is appended to $GITHUB_ENV
 with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
-    env_file.write('GITHUB_ENV=true\n')
+    env_file.write('GITHUB_ENV=false\n')
 
 # Uncomment this line to simulate the condition where GITHUB_ENV=false is appended to $GITHUB_ENV
 # with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
